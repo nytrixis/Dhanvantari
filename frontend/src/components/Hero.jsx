a807fc38c8455ff7ca58f02aa1faff5f0f9778f1
savarniknandinee <80510308+savarniknandinee@users.noreply.github.com>
2024-06-29 13:43:31 +0530

    #3

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -21,7 +21,15 @@ const Hero = () => {
         <div className="container relative" ref={parallaxRef}>
           <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
             <h1 className="h1 mb-6">
-              Expert Diagnosis, Rapid Emergency Response, Powered by Avalanche {` `}
+              Expert Diagnosis, <br /> Rapid Emergency Response, <br /> Powered by {" "}
+              <a
+                href="https://www.avax.network/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-n-1 underline hover:text-purple-500"
+              >
+                Avalanche
+              </a>
             </h1>
             <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
               Leverage the benefits of Expert diagnosis, virtual consultations, ambulance requests, AI chatbot, secure document transfer, ensuring comprehensive healthcare solutions.
